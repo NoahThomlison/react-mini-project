f74f7fd08b8a6027e89519f93279494f6810ed5f
noahthomlison <noahthomlison@gmail.com>
2021-12-30 15:45:41 -0800

    initial commit of work before watching tutorial

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -2,8 +2,22 @@ import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = (props) => {
+    const id = props.review.id
+    const name = props.review.name
+    const job = props.review.job
+    const image = props.review.image
+    const text = props.review.text
+
+  console.log(props.review.name)
+  return (
+    <div className='review'>
+      <img src={image} alt={name}></img>
+      <h2>{name}</h2>
+      <h3>{job}</h3>
+      <p>{text}</p>
+    </div>
+  )
 };
 
 export default Review;
