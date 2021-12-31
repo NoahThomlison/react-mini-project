0a8b356eae7bbefd49abdd6a04d68b3af8a440fe
noahthomlison <noahthomlison@gmail.com>
2021-12-31 15:59:17 -0800

    initial commit of prelim work before viewing tutorial

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = (props) => {
+  return (
+    <button className='filter-btn'>{props.catagory}</button>
+  )
 };
 
 export default Categories;
