8a5c576eaa06a9486ee272eaa92e724a85437f17
noahthomlison <noahthomlison@gmail.com>
2021-12-30 20:42:06 -0800

    initial commit of own work before reviewing tutorial and refactoring

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ( props ) => {
+  const { question: { id, info, title }} = props;
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpaned = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <div className='question'>
+      <div>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => toggleExpaned()}>
+        {expanded ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </div>
+      {expanded ? <p>{info}</p> : <p></p>}
+    </div>
+  )
 };
 
 export default Question;
+
