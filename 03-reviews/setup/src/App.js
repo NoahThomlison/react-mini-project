6cc966f880952c62bd4014efaa76c3cfc8a92e94
noahthomlison <noahthomlison@gmail.com>
2021-12-30 16:30:14 -0800

    moves buttons, data, state out of app.js and into review.js

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import Review from './Review';
-import reviews from "./data.js"
-import {useState} from 'react'
-import {  } from "react-icons/fa";
 
 function App() {
-  const [review, setReview] = useState(reviews[0])
-
-  const next = (id) => {
-    if(id + 1 > reviews.length){
-      return(0)
-    }
-    return(id)
-  }
-
-  const previous = (id) => {
-    if(id - 1 < 1){
-      return(reviews.length - 1)
-    }
-    return(id-2)
-  }
 
   return (
     <main>
@@ -28,11 +10,8 @@ function App() {
           <h2>Our Reviews</h2>
           <div className='underline'></div>
         </div>
-        <div className='review'>
-          <Review  review={review}/>
-          <button onClick={() => setReview(reviews[previous(review.id)])}>Previous</button>
-          <button onClick={() => setReview(reviews[Math.floor(Math.random() * reviews.length)])}>Random</button>
-          <button onClick={() => setReview(reviews[next(review.id)])}>Next</button>
+        <div>
+          <Review/>
         </div>
       </section>
   </main>
