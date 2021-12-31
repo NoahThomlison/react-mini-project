1c06e8d2127c54107bc5450c0dce300582bf0b63
noahthomlison <noahthomlison@gmail.com>
2021-12-30 16:30:34 -0800

    moves buttons, data, state out of app.js into reivew.js, adds more styling

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,20 +3,41 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = (props) => {
-    const id = props.review.id
-    const name = props.review.name
-    const job = props.review.job
-    const image = props.review.image
-    const text = props.review.text
 
-  console.log(props.review.name)
+    const [index, setIndex] = useState(0)
+    const {id, name, job, image, text} = people[index]
+    console.log(id)
+    const next = (id) => {
+      if(id + 1 > index.length){
+        return(0)
+      }
+      return(id)
+    }
+  
+    const previous = (id) => {
+      if(id - 1 < 1){
+        return(index.length - 1)
+      }
+      return(id-2)
+    }
+
   return (
-    <div className='review'>
-      <img src={image} alt={name}></img>
-      <h2>{name}</h2>
-      <h3>{job}</h3>
-      <p>{text}</p>
-    </div>
+    <article className='review'>
+      <div className="img-container">
+        <img className="person-img" src={image} alt={name}></img>
+        <span className='quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className="prev-btn" onClick={() => setIndex(people[previous(index.id)])}><FaChevronLeft/></button>
+        <button className="random-btn" onClick={() => setIndex(people[Math.floor(Math.random() * index.length)])}>Random</button>
+        <button className="next-btn"  onClick={() => setIndex(people[next(index.id)])}><FaChevronRight/></button>
+      </div>
+    </article>
   )
 };
 
