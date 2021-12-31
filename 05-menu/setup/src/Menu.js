1179dabc23d0b8daa57043ef7b097d1e430d9163
noahthomlison <noahthomlison@gmail.com>
2021-12-31 15:59:30 -0800

    initial commit of prelim work before viewing tutorial

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+
+  return (
+    <section className='menu-item'>
+    {items.map((item) => 
+    <header>
+      <h4>{item.title}</h4>
+      <h5 className='price'>{item.price}</h5>
+      <img className="photo" src={item.img} alt={item.name}></img>
+      <p className='item-info'>{item.desc}</p>
+    </header>
+
+    )}
+    </section>
+  );
 };
 
 export default Menu;
