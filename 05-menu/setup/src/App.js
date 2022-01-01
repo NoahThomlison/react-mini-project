3e26d0d81e32c6a8cdf63de12305cad2aa628969
noahthomlison <noahthomlison@gmail.com>
2021-12-31 17:35:28 -0800

    adds state varialbes, adds new function to toggle filter, changes html

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,20 +4,34 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const [filter, setFilter] = useState("All")
+  const [menu, setMenu] = useState(items)
+
+  const changeType = (category) => {
+    setFilter(category)
+    if(category === "all"){
+      setMenu(items)
+      return
+    }
+    const newItems = items.filter(item => item.category === category)
+    setMenu(newItems)
+  }
+
   return (
     <main>
       <header className='title'>
         <h3>Our Menu</h3>
-        <h3 className='underline'></h3>
+        <div className='underline'></div>
       </header>
       <section className='btn-container'>
-        <Categories catagory="All"/>
-        <Categories catagory="Breakfast"/>
-        <Categories catagory="Lunch"/>
-        <Categories catagory="Shakes"/>
+        <Categories changeType={changeType} category="all"/>
+        <Categories changeType={changeType} category="breakfast"/>
+        <Categories changeType={changeType} category="lunch"/>
+        <Categories changeType={changeType} category="shakes"/>
       </section>
+
       <section className='section-center'>
-        <Menu items={items}/>
+        <Menu items={menu} filter={filter}/>
       </section>
     </main>
   )
