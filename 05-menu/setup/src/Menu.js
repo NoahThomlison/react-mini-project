83f2c45b610d89b1697d9b827001829d35809fcc
noahthomlison <noahthomlison@gmail.com>
2021-12-31 17:36:26 -0800

    refactors htmls

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Menu = ({items}) => {
-
+const Menu = ({items, filter}) => {
+  
   return (
-    <section className='menu-item'>
+    <div>
     {items.map((item) => 
-    <header>
-      <h4>{item.title}</h4>
-      <h5 className='price'>{item.price}</h5>
-      <img className="photo" src={item.img} alt={item.name}></img>
-      <p className='item-info'>{item.desc}</p>
-    </header>
-
+      <header className='menu-item'>
+        <h4>{item.title}</h4>
+        <h4>{item.category}</h4>
+        <h5 className='price'>{item.price}</h5>
+        <img className="photo" src={item.img} alt={item.name}></img>
+        <p className='item-info'>{item.desc}</p>
+      </header>
     )}
-    </section>
+    </div>
   );
 };
 
