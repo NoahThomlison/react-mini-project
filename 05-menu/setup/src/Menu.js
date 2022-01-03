4e8ce3e5f513582da290f72dff8aada024e5ff04
noahthomlison <noahthomlison@gmail.com>
2022-01-02 16:10:03 -0800

    removes temperay item.category header used for debugging

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -7,7 +7,6 @@ const Menu = ({items, filter}) => {
     {items.map((item) => 
       <header className='menu-item'>
         <h4>{item.title}</h4>
-        <h4>{item.category}</h4>
         <h5 className='price'>{item.price}</h5>
         <img className="photo" src={item.img} alt={item.name}></img>
         <p className='item-info'>{item.desc}</p>
