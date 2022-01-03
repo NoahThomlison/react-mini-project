f3c75833732111f72de16892770720fde46727d7
noahthomlison <noahthomlison@gmail.com>
2022-01-02 16:51:24 -0800

    refactors code to remove categoryFinder function and replace with new Set line, changes html, refactors Category implimentation to move maping to Category component

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,13 +3,15 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const categories = ["All", ...new Set(items.map((item) => item.category))]
+
 function App() {
   const [filter, setFilter] = useState("All")
   const [menu, setMenu] = useState(items)
 
   const changeType = (category) => {
     setFilter(category)
-    if(category === "all"){
+    if(category === "All"){
       setMenu(items)
       return
     }
@@ -17,30 +19,14 @@ function App() {
     setMenu(newItems)
   }
 
-  const categoryFinder = (items) => {
-    const categories = []
-    items.forEach(item => {
-        if(!categories.includes(item.category)){
-          categories.push(item.category)
-        }
-    });
-    return(categories)
-  }
-
-  const categories = categoryFinder(items)
-
   return (
     <main>
-      <header className='title'>
-        <h3>Our Menu</h3>
-        <div className='underline'></div>
-      </header>
-      <section className='btn-container'>
-        <Categories key={99} changeType={changeType} category="all"/>
-        {categories.map((item) => 
-          <Categories key={categories.indexOf(item)} changeType={changeType} category={item}/>)}
-      </section>
-      <section className='section-center'>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories categories={categories} menu={menu} changeType={changeType}/>
         <Menu items={menu} filter={filter}/>
       </section>
     </main>
