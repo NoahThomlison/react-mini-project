20bdc2d7853fdeaf32feb8ba64b0f6aabeb577be
noahthomlison <noahthomlison@gmail.com>
2022-08-28 18:28:32 -0700

    adds conditional rendeirng for if error is preesent

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -29,7 +29,8 @@ function App() {
       type="text" 
       value={color} 
       onChange={(e) => setColor(e.target.value)} 
-      placeholder="#f15025"/>
+      placeholder="#f15025"
+      className={`${error? "error": null}`}/>
       <button className='btn' type="submit">Submit</button>
     </form>
   </section>
