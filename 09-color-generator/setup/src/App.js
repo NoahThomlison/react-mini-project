a21013f74ce3cc4d95ea7b8273415101a761e073
noahthomlison <noahthomlison@gmail.com>
2022-08-28 18:44:08 -0700

    sets up map for the colors returned

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -12,6 +12,7 @@ function App() {
     event.preventDefault()
     try{
       let colors = new Values(color).all(10)
+      setList(colors)
       setError(false)
       console.log(colors)
     }
@@ -36,7 +37,9 @@ function App() {
     </form>
   </section>
   <section className='colors'> 
-    <h4>List goes heere</h4> 
+    {list.map((color, index) => {
+      return <SingleColor key={index} {...color} index={index}></SingleColor>
+    })}
   </section>
   </>
   )}
