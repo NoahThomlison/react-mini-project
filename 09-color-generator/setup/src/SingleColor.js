0a23374c590a16794f0a5c3a3543073be2c4badb
noahthomlison <noahthomlison@gmail.com>
2022-08-29 21:31:53 -0700

    udpates text to be light if dark shade

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,7 +5,7 @@ const SingleColor = ({rgb, weight, index, hex}) => {
   const [alert, setAlert] = useState(false)  
   const bcg = rgb.join(", ")
   return (
-    <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
+    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hex}</p>
     </article>
