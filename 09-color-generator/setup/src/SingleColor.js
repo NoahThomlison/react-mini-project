8703f0c651596647860a087672240933e334b4c6
noahthomlison <noahthomlison@gmail.com>
2022-08-29 21:56:06 -0700

    sets up copy paste functionality with timer for clearing alert

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,10 +5,22 @@ const SingleColor = ({rgb, weight, index, hex}) => {
   const [alert, setAlert] = useState(false)  
   const bcg = rgb.join(", ")
   const hexValue = `#${hex}`
+  useEffect(() => {
+    const timer = setTimeout(()=> {
+      (setAlert(false))}, 3000)
+      return(()=> clearTimeout(timer))
+  }, [alert])
+
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
+    <article onClick={() => {
+      setAlert(true)
+      navigator.clipboard.writeText(hexValue)
+      }} 
+      className={`color ${index > 10 && 'color-light'}`} 
+      style={{backgroundColor:`rgb(${bcg})`}}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>Copied to Clipboard!</p>}
     </article>
   )
 }
