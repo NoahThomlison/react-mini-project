3ace42afc201f5ee1adb571646bef9258582a66b
noahthomlison <noahthomlison@gmail.com>
2022-08-31 07:23:49 -0700

    adds basic functionality to alert

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({alert}) => {
+  const message = alert.msg
+  const type = alert.type
+  return (
+    <section className='alert'>
+      <h4 className={type}>{message}</h4>
+    </section>
+  )
 }
 
 export default Alert
