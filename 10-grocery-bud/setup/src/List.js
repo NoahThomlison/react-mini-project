2a71b9522f97de94b5737200696a1eb35a0a1ba0
noahthomlison <noahthomlison@gmail.com>
2022-08-30 20:40:38 -0700

    build list component to let it map trhoguh the various list

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({groceryList}) => {
+
+  return (
+    <section>
+      {groceryList.map((item) => {
+        return(
+          <section>
+            <h1>{item}</h1>
+            <FaEdit></FaEdit>
+            <FaTrash></FaTrash>
+          </section>
+        )
+      })}
+    </section>
+  )
 }
 
 export default List
