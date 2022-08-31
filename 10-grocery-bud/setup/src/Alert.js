e6c2272fdcbeae15bd4d98d3919b4d722bceca72
noahthomlison <noahthomlison@gmail.com>
2022-08-31 07:48:43 -0700

    refactors styling

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -3,9 +3,7 @@ import React, { useEffect } from 'react'
 const Alert = ({msg, type}) => {
   console.log(msg, type)
   return (
-    <section className='alert'>
-      <h4 className={type}>{msg}</h4>
-    </section>
+      <p className={`alert ${type}`}>{msg}</p>
   )
 }
 
