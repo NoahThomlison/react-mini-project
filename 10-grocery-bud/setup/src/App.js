83fecde2d8cb3175e0b7cc5f72e9eb425529f70e
noahthomlison <noahthomlison@gmail.com>
2022-08-30 20:31:46 -0700

    sets up basic component with state varialbe and change and submit handlers

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,21 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [groceryList, setGroceryList] = useState([])
+  const [groceryItem, setGroceryItem] = useState("")
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setGroceryList([...groceryList, groceryItem])
+  }
+
+  return (
+    <article>
+      <h2>Grocery Bud</h2>
+      <input value={groceryItem} onChange={(e) => setGroceryItem(e.target.value)} type="text" placeholder='e.g. eggs' />
+      <button onClick={(e) => onSubmit(e)}>Submit</button>
+    </article>
+  )
 }
 
 export default App
